3c95e783e8843063db975d62de224caa3395f380
Yauheniya <yauheniya.varabyova@adesso.de>
2026-04-29 20:36:40 +0200

    content: Add React content and refactor the elements

--- src/data/react-content.js
+++ src/data/react-content.js
@@ -1,6 +1,10 @@
 // src/data/react-content.js
 import ReactIntroduction from './react/introduction.mdx';
 import ReactComponents from './react/components.mdx';
+import ReactProps from './react/props.mdx';
+import ReactEvents from './react/events.mdx';
+import ReactState from './react/state.mdx';
+import ReactHooks from './react/hooks.mdx';
 
 export const reactContent = {
   icon: "logos:react",
@@ -15,6 +19,26 @@ export const reactContent = {
       id: "react-components",
       title: "Components",
       Content: ReactComponents
+    },
+    {
+      id: "react-props",
+      title: "Props",
+      Content: ReactProps
+    },
+    {
+      id: "react-events",
+      title: "Events",
+      Content: ReactEvents
+    },
+    {
+      id: "react-state",
+      title: "State",
+      Content: ReactState
+    },
+    {
+      id: "react-hooks",
+      title: "Hooks",
+      Content: ReactHooks
     }
   ]
 };
